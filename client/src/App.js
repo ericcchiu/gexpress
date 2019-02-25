91045f74d42f1f559f6775c7cfe1813ba6fcac72
ericcchiu <eric.chn.chiu@gmail.com>
2019-02-25 16:55:40 -0600

    (feature) Add header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Header from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to React Boiler Plate</h1>
-      </div>
-    )
+      <Router>
+        <div>
+          <h1>Welcome to React Boiler Plate</h1>
+          <Header />
+        </div>
+      </Router>
+    );
   }
 }
 export default App; 
