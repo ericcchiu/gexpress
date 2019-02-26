67b8e6886ac4200894ef638f0f669a54ea862d7c
ericcchiu <eric.chn.chiu@gmail.com>
2019-02-25 18:16:04 -0600

    (bugfix) Fix bug where component does not change when routed.

---
+++ client/src/MainComponents/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Stores from '../StoreComponent/Stores';
+import Home from './Home';
+import Departments from '../DepartmentsComponent/Departments';
+import ShoppingList from '../ShoppingListComponent/ShoppingList';
+
+/*
+* The Main component renders the provided routes that matches. 
+*/
+
+const Main = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/stores' component={Stores} />
+        <Route exact path='/departments' component={Departments} />
+        <Route exact path='/shoppinglist' component={ShoppingList} />
+
+      </Switch>
+    </main>
+  );
+}
+export default Main;
