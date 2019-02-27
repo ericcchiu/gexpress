b750a41531d8084bf41f3b47ab6e2b3e22bdd157
ericcchiu <eric.chn.chiu@gmail.com>
2019-02-26 20:21:54 -0600

    (feature) Add quick reorder page.

---
+++ client/src/QuickReorderComponent/QuickReorder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuickReorder = () => {
+  return (
+    <div>
+      <h1>Welcome to Quick Reorder Component</h1>
+    </div>
+  );
+}
+
+export default QuickReorder;
