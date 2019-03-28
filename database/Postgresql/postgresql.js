a5f9f0321a18f979a1b9c2f8c5de754fd190b4bc
Eric Chiu <37917779+ericcchiu@users.noreply.github.com>
2019-03-27 22:20:25 -0500

    Update postgresql.js

--- database/Postgresql/postgresql.js
+++ database/Postgresql/postgresql.js
@@ -1,4 +1,13 @@
-const postgresConnection = require('knex')({
-  client: 'pg',
+require("dotenv").config();
 
-})
+const postgresConnection = require("knex")({
+  client: "pg",
+  connection: {
+    user: process.env.PSQL_USER || "student",
+    host: process.env.PSQL_HOST || "127.0.0.1",
+    password: process.env.PSQL_PASSWORD || "student",
+    database: process.env.PSQL_DATABASENAME || "projects"
+  }
+});
+
+module.exports = postgresConnection;
